f3bbf7e3a2c39cf4667f9eb0121699e55782f92c
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-15 14:51:07 -0500

    Add middleware router to server.js and generic / GET endpoint to test api is up

--- api/server.js
+++ api/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 
+const AccountsRouter = require('./accounts/accounts-router');
+
 const server = express();
 
 server.use(express.json());
+server.use('/api/router', AccountsRouter)
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: "up" });
+});
 
 module.exports = server;
